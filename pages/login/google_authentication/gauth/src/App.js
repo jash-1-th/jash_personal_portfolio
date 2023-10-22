ab26691b0102914bde4db511173d0ef789e12aa8
Jash <y.j.naiduofficial@gmail.com>
2023-10-22 20:08:32 +0530

    working signin just generating JWT

--- pages/login/google_authentication/gauth/src/App.js
+++ pages/login/google_authentication/gauth/src/App.js
@@ -1,23 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+/* global google */
+
+// import logo from "./logo.svg";
+import { useEffect ,useState } from "react";
+import "./App.css";
 
 function App() {
+
+
+
+
+
+  function handleCallbackResponse(response) {
+    console.log("Encoded JWT ID token : " + response.credential);
+  }
+
+  useEffect(() => {
+    google.accounts.id.initialize({
+      client_id:
+        "445940879152-rhvkp5ckav4vgfjrbuav5mngk6apercq.apps.googleusercontent.com",
+      callback: handleCallbackResponse,
+    });
+
+    google.accounts.id.renderButton(document.getElementById("siginInDiv"), {
+      theme: "outline",
+      size: "large",
+    });
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div id="siginInDiv"></div>
     </div>
   );
 }
